ce8668612ecba276d288120a0af20b9c3a25312b
smannan <smannan95@hotmail.com>
2018-05-29 18:45:09 -0700

    fixed regex in predict transition

--- Pipeline/Predict_Transitions.py
+++ Pipeline/Predict_Transitions.py
@@ -61,10 +61,30 @@ def re_match_lists_helper(pattern_list, word_list):
     return True
 
 def re_match_lists(pattern_list_list, word_list):
+    # for each pattern
     for pl in range(len(pattern_list_list)):
+        # match the specific pattern against the word
         if (re_match_lists_helper(pattern_list_list[pl], word_list)):
             return True
     return False
+
+def re_find_lists(pattern_list_list, word):
+    res = []
+
+    for i in range(len(pattern_list_list)):
+        #print (word, ' '.join(pattern_list_list[i]))
+
+        p = re.compile(' '.join(pattern_list_list[i]))
+
+        res += p.findall(word)
+
+    if len(res) == 0:
+        return ''
+
+    return ' '.join(res)
+
+def find_any_pattern(word, pattern_list):
+    return re_find_lists(pattern_list, word)
 
 def matches_any_4_word_pattern(word1, word2, word3, word4):
     pattern_list_list = [bill_id_pattern_4_1, bill_id_pattern_4_2]
@@ -110,81 +130,101 @@ def shift_words_over(words, word_ix, shift_amount):
         
     return words
 
-def replace_bill_ids_in_utterance(utterance, last_bill_number, t1, t2, t3, t4):
+# returns all the matched bill names found in utterance
+def find_bill_names(utterance):
+    input_list = utterance.lower().split()
+    pattern_list_list = []
+    res = []
+
+    for n in range(1,5):
+        ngrams = (list(zip(*[input_list[i:] for i in range(n)])))
+
+        if n == 4: 
+            pattern_list_list = [bill_id_pattern_4_1, bill_id_pattern_4_2]
+
+        elif n == 3:
+            pattern_list_list = [bill_id_pattern_3_1, bill_id_pattern_3_2]
+
+        elif n == 2:
+            pattern_list_list = [bill_id_pattern_2_1, bill_id_pattern_2_2,
+             bill_id_pattern_2_3, bill_id_pattern_2_4,
+             bill_id_pattern_2_5, bill_id_pattern_2_6,
+             bill_id_pattern_2_7, bill_id_pattern_2_8,
+             bill_id_pattern_2_9, bill_id_pattern_2_10]
+
+        elif n == 1:
+            pattern_list_list = [bill_id_pattern_1_1, bill_id_pattern_1_2,
+             bill_id_pattern_1_3, bill_id_pattern_1_4, bill_id_pattern_1_5,
+             bill_id_pattern_1_6, bill_id_pattern_1_7, bill_id_pattern_1_8,
+             bill_id_pattern_1_9, bill_id_pattern_1_10]
+        
+        for word in ngrams:
+            target = ' '.join(word).strip()
+            bill_names = find_any_pattern(target, pattern_list_list)
+
+            if len(bill_names) > 0: res.append(bill_names)
+    
+    return res
+
+# utterance text
+def replace_bill_ids_in_utterance(utterance):
+    # split text into list of words
     words = utterance.lower().split()
     utterance_length = len(words)
-    word_ix = 0
-    bill_id_replaced = False
+
+    word_ix = 0 # index into the utterance
+    bill_id_replaced = False # if something has been replaced
+
+    # going through each word
     while(word_ix < utterance_length):
+        # match a four word pattern
         if (word_ix < (utterance_length-3) and
             matches_any_4_word_pattern(words[word_ix],
                                          words[word_ix+1],
                                          words[word_ix+2],
                                          words[word_ix+3])):
-            last_bill_number = words[word_ix+3]
+
             words[word_ix] = "<BILL_ID>"
             words = shift_words_over(words, word_ix+1, 3)
             utterance_length -= 3
             bill_id_replaced = True
-            t4 += 1
+            
+        # match a three word pattern
         elif (word_ix < (utterance_length-2) and
               matches_any_3_word_pattern(words[word_ix],
                                          words[word_ix+1],
                                          words[word_ix+2])):
-            last_bill_number = words[word_ix+2]
+            
             words[word_ix] = "<BILL_ID>"
             words = shift_words_over(words, word_ix+1, 2)
             utterance_length -= 2
             bill_id_replaced = True
-            t3 += 1
+            
+        # match a two word pattern
         elif (word_ix < (utterance_length-1) and
             matches_any_2_word_pattern(words[word_ix],
                                          words[word_ix+1])):
-            last_bill_number = words[word_ix+1]
+            
             words[word_ix] = "<BILL_ID>"
             words = shift_words_over(words, word_ix+1, 1)
             utterance_length -= 1
             bill_id_replaced = True
-            t2 += 1
+            
+        # match a one word pattern
         elif (matches_any_1_word_pattern(words[word_ix])):
-            last_bill_number = words[word_ix].split("[a-z]+")[-1]
             words[word_ix] = "<BILL_ID>"
             bill_id_replaced = True
-            t1 += 1
+            
 
         word_ix += 1
             
-    return (" ".join(words), last_bill_number, bill_id_replaced, t1, t2, t3, t4)
+    return (" ".join(words), bill_id_replaced)
 
 def replace_bill_ids(old, new):
-    t1 = 0  #keeps track of how many bill id replacements there were
-    t2 = 0
-    t3 = 0
-    t4 = 0
-    
-    last_bill_number = ""
-    last_bill_number_line = 0
-    transition_window_list = []
-    line_number = 0
     for line in old:
         line_splits = line.lower().rstrip("\n").split("~")
-        
-        (new_text, current_bill_number, bill_id_replaced, t1, t2, t3, t4) = replace_bill_ids_in_utterance(line_splits[2], last_bill_number, t1, t2, t3, t4)
-        
-        if (bill_id_replaced):
-            if (current_bill_number != last_bill_number):
-                transition_window_list.append((last_bill_number_line, line_number))
-                last_bill_number = current_bill_number
-                last_bill_number_line = line_number
-            elif (current_bill_number == last_bill_number):
-                last_bill_number_line = line_number
-        
+        (new_text, bill_id_replaced) = replace_bill_ids_in_utterance(line_splits[2])
         new.write(line_splits[0] + "~" + line_splits[1] + "~" + new_text + "~" + line_splits[3] + "\n")
-        line_number += 1
-        
-    #print("Length of Bill Patterns Replaced\n1: " + str(t1) + "\n2: " + str(t2) + "\n3: " + str(t3) + "\n4: " + str(t4))
-    return transition_window_list
-
 
 # Define add context function (prefix and postfix words in surrounding utterances).
 # adds the prefix POST to all utterances n after
@@ -246,45 +286,35 @@ def predict_from_neural_network():
 
 # new_transcript is a list of dictionaries
 def process_raw_data(new_transcript):
-	### Determine the raw .srt file that will be processed.
+    ### Determine the raw .srt file that will be processed.
 
-	cleaned_raw_filename = "cleaned_transcript.csv"
-	cleaned_raw_bill_id_replaced_filename = cleaned_raw_filename + "2"
+    cleaned_raw_filename = "cleaned_transcript.csv"
+    cleaned_raw_bill_id_replaced_filename = cleaned_raw_filename + "2"
 
-	print("...Parsing raw transcript...")
+    print("...Parsing raw transcript...")
 
-	cleaned_raw = pd.DataFrame(new_transcript)
-	cleaned_raw.sort_values(["video_id", "start"]) \
-	 .to_csv(cleaned_raw_filename, sep="~", index=False)
+    cleaned_raw = pd.DataFrame(new_transcript)
+    cleaned_raw.sort_values(["video_id", "start"]) \
+     .to_csv(cleaned_raw_filename, sep="~", index=False)
 
-	print("...Raw transcript parsed, beginning text formatting and bill replacement...")
+    print("...Raw transcript parsed, beginning text formatting and bill replacement...")
 
-	transition_window_list = [] #not currently used, but is available for use
+    with open(cleaned_raw_filename, 'r') as old:
+        with open(cleaned_raw_bill_id_replaced_filename, 'w') as new:
+            # consume/write headings
+            h = old.readline()
+            new.write(h)
+            
+            replace_bill_ids(old, new)
 
-	with open(cleaned_raw_filename, 'r') as old:
-	    with open(cleaned_raw_bill_id_replaced_filename, 'w') as new:
-	        # consume/write headings
-	        h = old.readline()
-	        new.write(h)
-	            
-	        #actually iterate through the file
-	        transition_window_list = replace_bill_ids(old, new)
+    print("...Text formatted and bills replaced, adding context...")
 
-	print("...Text formatted and bills replaced, adding context...")
+    add_context(5, cleaned_raw_bill_id_replaced_filename)
 
-	add_context(5, cleaned_raw_bill_id_replaced_filename)
+    print("...Context added.")
 
-	print("...Context added.")
-
-	processed_new_transcript = pd.read_csv(cleaned_raw_bill_id_replaced_filename, sep="~")
-	return processed_new_transcript
-
-# returns None if no bill name found, returns bill name if it is found
-def extract_bill_name(text):
-    print("edited text: ")
-    print(textedit.correct_text(list(text)))
-    return None
-
+    processed_new_transcript = pd.read_csv(cleaned_raw_bill_id_replaced_filename, sep="~")
+    return processed_new_transcript
 
 def enhance_dictionary_helper(combined): 
     enhanced_dictionary = []
@@ -304,8 +334,9 @@ def enhance_dictionary_helper(combined):
             i += 1
             continue
         
-        #otherwise, if there is a bill id immediately associated with the utterance,
-        #input it into the dictionary, then continue
+        # otherwise, if there is a bill id immediately associated with the utterance,
+        # input it into the dictionary, then continue
+        
         if (len(bill_names_list[i]) > 0):
             enhanced_dictionary.append({
                     'start':start_list[i], 'end':end_list[i],
@@ -313,7 +344,7 @@ def enhance_dictionary_helper(combined):
                     'suggested_bill':bill_names_list[i][0]})
             i += 1
             continue
-        
+
         #otherwise, scan all table rows until the next transition utterance looking for
         #an associated bill id
         j = i+1
@@ -343,26 +374,48 @@ def enhance_dictionary_helper(combined):
             
     return enhanced_dictionary
 
+def collapse_dictionary(enhanced_dictionary):
+    res = []
+    i = 0
+    j = 0
+    n = len(enhanced_dictionary)
+    epsilon = 5
+
+    while i < n:
+        j = i + 1
+        res.append(enhanced_dictionary[i])
+
+        while j < n and math.fabs(enhanced_dictionary[j]['start']
+         - enhanced_dictionary[i]['end']) < epsilon:
+            j += 1
+            i += 1
+
+        i = j
+
+    return res
 
 def enhance_dictionary(original_transcript, transition_dictionary):
     #transition_dictitionary format: [{video_id, start, end, text}]
-    #original transcript format new_transcript = [{'start':'1', 'end':'9', 'text':'we are starting', 'video_id':'1'}]
+    #original transcript format new_transcript = [{'start':'1', 'end':'9', 
+    #'text':'we are starting', 'video_id':'1'}]
     
     original = pd.DataFrame.from_dict(original_transcript)
     transitions = pd.DataFrame.from_dict(transition_dictionary)
     
     #combined = pd.merge(original, transitions, on=["video_id", "start", "end"], suffixes=("_original", "_modified"))
-    combined = pd.merge(original, transitions, how='left', on=["video_id", "start", "end"], suffixes=("_original", "_modified")) 
+    combined = pd.merge(original, transitions, 
+     how='left', on=["video_id", "start", "end"], suffixes=("_original", "_modified")) 
     substituted_original = textedit.correct_text(list(combined["text_original"]))
     
     bill_names_in_text = []
     for text in substituted_original:
-        bill_names_in_text.append(re.findall("[a-zA-Z]+\\s*[0-9]+", text))
+        bills = find_bill_names(text)
+        bill_names_in_text.append(bills)
 
     combined["bill_names"] = bill_names_in_text
     
-    return enhance_dictionary_helper(combined)
-    
+    #return enhance_dictionary_helper(combined)
+    return collapse_dictionary(enhance_dictionary_helper(combined))
     
 # given a list of dictionaries, return a 
 def predict(new_transcript):
@@ -385,7 +438,8 @@ def predict(new_transcript):
     
 
 def main():
-    new_transcript = pd.read_csv('../data/cleaned/transcript_witheld.csv', sep="~").to_dict('records')
+    new_transcript = pd.read_csv('../data/cleaned/transcript_witheld.csv', sep="~") \
+        .to_dict('records')
     transition_dictionary = predict(new_transcript)
     enhanced_dictionary = enhance_dictionary(new_transcript, transition_dictionary)
     
@@ -398,6 +452,9 @@ def main():
     
     print("number that have a suggested bill: ")
     print(i)
+
+    #test = 'I urge a strong aye vote on assembly bill number 329 ab-329.'
+    #find_bill_names(test)
     
 if __name__ == "__main__":
     main()
